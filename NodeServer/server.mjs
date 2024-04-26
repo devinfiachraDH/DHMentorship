dcda44cbbba1b71ba7170ae619d31da9550ddb96
devinfiachraDH <153081660+devinfiachraDH@users.noreply.github.com>
2024-04-26 13:09:59 +0200

    serving pokemon names to the frontend

--- NodeServer/server.mjs
+++ NodeServer/server.mjs
@@ -1,10 +1,23 @@
 import express from "express";
+import axios from "axios";
 
 const app = express();
 const port = 3000;
 
+const url = 'https://pokeapi.co/api/v2/pokemon?limit=151';
+
 app.get("/", (req, res) => {
     res.send("The beginning of my server project!")
+})
+
+app.get("/pokemon", (req, res) => {
+    axios.get(url)
+    .then(data => {
+      res.json(data.data.results);
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'Internal Server Error' });
+    });
 })
 
 app.listen(port, () => {

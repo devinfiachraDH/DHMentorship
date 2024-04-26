8534f06743eb411604cf1e23cd7d3531c2d0ae86
devinfiachraDH <153081660+devinfiachraDH@users.noreply.github.com>
2024-04-26 15:09:26 +0200

    update server with async await, install nunjucks

--- NodeServer/server.mjs
+++ NodeServer/server.mjs
@@ -1,8 +1,15 @@
 import express from "express";
 import axios from "axios";
+import { writeFileSync } from "fs";
+import nunjucks from "nunjucks";
 
 const app = express();
 const port = 3000;
+
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app
+});
 
 const url = 'https://pokeapi.co/api/v2/pokemon?limit=151';
 
@@ -10,51 +17,24 @@ app.get("/", (req, res) => {
     res.send("The beginning of my server project!")
 })
 
-app.get("/pokemon", (req, res) => {
-    axios.get(url)
-    .then(data => {
-        const pokemonUrls = data.data.results.map(pokemon => {
-            console.log(pokemon.url);
-        });
-    })
-    .catch(error => {
-      res.status(500).json({ error: 'Internal Server Error' });
-    });
+app.get("/pokemon", async (req, res) => {
+   const getAllPokemonUrls = await axios.get(url)
+   const pokemonPromises = getAllPokemonUrls.data.results.map(pokemon => axios.get(pokemon.url));
+
+   const results = await Promise.all(pokemonPromises);
+   const pokemons = results.map((pokemon) => pokemon.data)
+
+   res.send(
+    pokemons.map((pokemon) => ({
+      name : pokemon.name,
+      id : pokemon.id,
+      sprite : pokemon.sprites.front_default
+   })));
+
+
+   //writeFileSync("pokemon.json", JSON.stringify(pokemons.map((pokemon) => pokemon.data)))
 })
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })
-
-/* CHAT GPT 
-
-need info from 
-"id", "name"
-"sprites": {
-  "front_default" : : "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/151.svg"
-}
-
-app.get("/pokemon", (req, res) => {
-    axios.get(initialUrl)
-    .then(response => {
-        const pokemonUrls = response.data.results.map(pokemon => pokemon.url);
-        const pokemonPromises = pokemonUrls.map(url => axios.get(url));
-
-        Promise.all(pokemonPromises)
-          .then(pokemonResponses => {
-              const pokemonData = pokemonResponses.map(response => response.data);
-              res.json(pokemonData);
-          })
-          .catch(error => {
-              console.error("Error fetching Pokemon data:", error);
-              res.status(500).json({ error: 'Internal Server Error' });
-          });
-    })
-    .catch(error => {
-        console.error("Error fetching Pokemon list:", error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    });
-});
-
-
-*/

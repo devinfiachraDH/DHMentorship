90866ce62254327c9a803ed5807180a02b4d81e2
devinfiachraDH <153081660+devinfiachraDH@users.noreply.github.com>
2024-06-07 13:20:42 +0200

    render pokemon with template, update css

--- NodeServer/server.mjs
+++ NodeServer/server.mjs
@@ -1,10 +1,18 @@
 import express from "express";
 import axios from "axios";
+import path from "path";
+import { fileURLToPath } from 'url';
 import { writeFileSync } from "fs";
 import nunjucks from "nunjucks";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 const port = 3000;
+
+// I will serve static files
+app.use(express.static(path.join(__dirname, 'public')))
 
 nunjucks.configure('views', {
   autoescape: true,
@@ -26,16 +34,21 @@ app.get("/pokemon", async (req, res) => {
    const results = await Promise.all(pokemonPromises);
    const pokemons = results.map((pokemon) => pokemon.data)
 
-   res.send(
-    pokemons.map((pokemon) => ({
+  //  res.send(
+  //   pokemons.map((pokemon) => ({
+  //     name : pokemon.name,
+  //     id : pokemon.id,
+  //     sprite : pokemon.sprites.front_default
+  //  })));
+  let pokemonData =  pokemons.map((pokemon) => ({
       name : pokemon.name,
       id : pokemon.id,
       sprite : pokemon.sprites.front_default
-   })));
-
+   }))
 
    //writeFileSync("pokemon.json", JSON.stringify(pokemons.map((pokemon) => pokemon.data)))
-})
+    res.render('pokemon.html', {pokemonData})
+  })
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)

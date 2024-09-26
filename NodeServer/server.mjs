6dc2509f4778a47354d6a013ac0efc5bc0dfef53
devinfiachraDH <153081660+devinfiachraDH@users.noreply.github.com>
2024-09-26 16:15:18 +0200

    add poke menu, begin pagination

--- NodeServer/server.mjs
+++ NodeServer/server.mjs
@@ -19,7 +19,10 @@ nunjucks.configure('views', {
   express: app
 });
 
-const url = 'https://pokeapi.co/api/v2/pokemon?limit=151';
+const instance = axios.create({
+  baseURL: 'https://pokeapi.co/api/v2'
+});
+
 
 app.set('view engine', 'html')
 
@@ -28,7 +31,7 @@ app.get("/", (req, res) => {
 })
 
 app.get("/pokemon", async (req, res) => {
-   const getAllPokemonUrls = await axios.get(url)
+   const getAllPokemonUrls = await instance.get(`/pokemon?limit=50&offset=${req.query.next}`)
    const pokemonPromises = getAllPokemonUrls.data.results.map(pokemon => axios.get(pokemon.url));
 
    const results = await Promise.all(pokemonPromises);
@@ -41,7 +44,7 @@ app.get("/pokemon", async (req, res) => {
   //     sprite : pokemon.sprites.front_default
   //  })));
   let pokemonData =  pokemons.map((pokemon) => ({
-      name : pokemon.name,
+      name : (pokemon.name).toUpperCase(),
       id : pokemon.id,
       sprite : pokemon.sprites.front_default
    }))
@@ -49,6 +52,19 @@ app.get("/pokemon", async (req, res) => {
    //writeFileSync("pokemon.json", JSON.stringify(pokemons.map((pokemon) => pokemon.data)))
     res.render('pokemon.html', {pokemonData})
   })
+
+app.get("/pokedex/:id", async (req, res) => {
+  
+  const getPokemonInfo = await instance.get(`/pokemon/${req.params.id}`)
+
+  let pokemonData = {
+    name : getPokemonInfo.data.name,
+    id : getPokemonInfo.data.id,
+    sprite : getPokemonInfo.data.sprites.front_default
+ }
+  
+  res.render("pokedex.html", {"pokemon": pokemonData});
+})
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
